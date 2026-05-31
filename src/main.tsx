8d4c863ed7670213fa836956bc068f7b8f6b2217
INIMONEY234 <inimoneytech@gmail.com>
2026-05-31 23:16:30 +0100

    new2 commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 
-const rootElement = document.getElementById("root");
-
-if (!rootElement) {
-  throw new Error("Root element not found");
-}
-
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
